7362a52abedf1ffc01d116dad6472c4093e15dc8
Jobzi <jipsonmurillo@gmail.com>
2021-05-12 13:29:12 -0500

    fist commit, add eslintconfig

---
+++ src/components/footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => (
+  <footer>
+    <div className="footer-wrapper">
+      Hola soy tu footer
+    </div>
+  </footer>
+)
+export default Footer
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+const Header = ({ name = 'name' }) => {
+  return (
+    <header>
+        <p>Heloda {name}</p>
+    </header>
+  )
+}
+
+export default Header
